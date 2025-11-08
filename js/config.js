66bd0969cd040d418877a9895fa62ce957bd9149
erika <erikajs616@gmail.com>
2025-11-08 14:13:25 +0700

    Update Firebase configuration and initialization

--- js/config.js
+++ js/config.js
@@ -1,14 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyC5H-Evp4sk1YHb_4IPfDg6CE8viDPg008",
-    authDomain: "input-nilai-44e9f.firebaseapp.com",
-    databaseURL: "https://input-nilai-44e9f-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "input-nilai-44e9f",
-    storageBucket: "input-nilai-44e9f.firebasestorage.app",
-    messagingSenderId: "361177984219",
-    appId: "1:361177984219:web:8b389ff668ba9aba8270fe",
-    measurementId: "G-L70W06XHRX"
+  apiKey: "AIzaSyBIzgv14G8Af1Ccy4_yIljBujCfJEXFEgc",
+  authDomain: "input-bfeb7.firebaseapp.com",
+  databaseURL: "https://input-bfeb7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "input-bfeb7",
+  storageBucket: "input-bfeb7.firebasestorage.app",
+  messagingSenderId: "422287140007",
+  appId: "1:422287140007:web:f2e755acb5dfa46abce782",
+  measurementId: "G-PTB9ZHXC6F"
 };
 
-export { firebaseConfig };
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { firebaseConfig, database};
+
